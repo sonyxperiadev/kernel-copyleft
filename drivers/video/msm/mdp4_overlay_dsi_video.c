5771b9bb054f6a6741ee7a2a7b9a07ec4915b4d3
Kuogee Hsieh <khsieh@codeaurora.org>
2012-09-27 11:44:15 -0700

    msm_fb: display: check pipes queued before enter wait_for_completion
    Timing generator will not be turned on if there have no pipes queued.
    Therefore wait_for_completion may never be completed if there have
    no pipes queued.
    
    Updating drivers/video/msm/mdp4_overlay_dsi_video.c
            drivers/video/msm/mdp4_overlay_lcdc.c
    for changes on top of Change-Id: Ie20097be3d6d37c19080c2e20bce747a1d103012
    
    Change-Id: Iafba086e4ba20b31470c9fd385c4f5c474ecadcf
    CRs-Fixed: 405421
    (cherry picked from commit c15016aa0986c491b33f12af4c62e3bcd5296a79)
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    	drivers/video/msm/mdp4_overlay_lcdc.c
    
    (cherry picked from commit 2729536376847b7d19b43a7e40cb4fa364cb9ef8)
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay_dsi_video.c
    	drivers/video/msm/mdp4_overlay_lcdc.c
    
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -1046,7 +1046,7 @@ void mdp4_dsi_video_overlay(struct msm_fb_data_type *mfd)
 	uint8 *buf;
 	unsigned int buf_offset;
 	int bpp;
-	int cndx = 0;
+	int cnt, cndx = 0;
 	struct vsycn_ctrl *vctrl;
 	struct mdp4_overlay_pipe *pipe;
 
@@ -1073,14 +1073,17 @@ void mdp4_dsi_video_overlay(struct msm_fb_data_type *mfd)
 
 	mdp4_overlay_mdp_perf_upd(mfd, 1);
 
+	cnt = 0;
 	mutex_lock(&mfd->dma->ov_mutex);
-	mdp4_dsi_video_pipe_commit(cndx, 0);
+	cnt = mdp4_dsi_video_pipe_commit(cndx,0);
 	mutex_unlock(&mfd->dma->ov_mutex);
 
-	if (pipe->ov_blt_addr)
-		mdp4_dsi_video_wait4ov(cndx);
-	else
-		mdp4_dsi_video_wait4dmap(cndx);
+	if (cnt) {
+		if (pipe->ov_blt_addr)
+			mdp4_dsi_video_wait4ov(cndx);
+		else
+			mdp4_dsi_video_wait4dmap(cndx);
+	}
 
 	mdp4_overlay_mdp_perf_upd(mfd, 0);
 }
