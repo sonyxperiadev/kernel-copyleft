67cb9610440c242a24bf255f34d7ba6bcdc483e1
Kuogee Hsieh <khsieh@codeaurora.org>
2013-01-10 13:28:21 -0800

    msm_fb: display: Do clock control within mdp4_hw_init
    Since mdp4_hw_init() access mdp registers, mdp clock control
    within mdp4_hw_init() is necessary.
    
    CRs-fixed: 455506
    Change-Id: I768f468314fc920029b4c88e49ef6a74f6b74f58
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdp4_util.c
+++ drivers/video/msm/mdp4_util.c
@@ -391,6 +391,7 @@ void mdp4_hw_init(void)
 	int i;
 	/* MDP cmd block enable */
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
+	mdp_clk_ctrl(1);
 
 	mdp_bus_scale_update_request(5);
 
@@ -463,6 +464,7 @@ void mdp4_hw_init(void)
 
 	/* MDP cmd block disable */
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
+	mdp_clk_ctrl(0);
 }
 
 
