b8d296f3a76adf5a31604c467836727c7b360679
Sangani Suryanarayana Raju <csuryan@codeaurora.org>
2013-02-11 17:55:18 +0530

    Change priority of kernel message to avoid message flooding in logs.
    kernel logs are flooded with info message from mdp4_overlay_mdp_perf_upd,
    so changed the priority of message to debug.
    
    Change-Id: I1ed9e7a372c563078a8127b324c05b90c67c5849
    Signed-off-by: Sangani Suryanarayana Raju <csuryan@codeaurora.org>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2861,7 +2861,7 @@ void mdp4_overlay_mdp_perf_upd(struct msm_fb_data_type *mfd,
 			mdp_bus_scale_update_request
 				(OVERLAY_BUS_SCALE_TABLE_BASE -
 				 perf_req->mdp_bw);
-			pr_info("%s mdp bw is changed [%d] from %d to %d\n",
+			pr_debug("%s mdp bw is changed [%d] from %d to %d\n",
 				__func__,
 				flag,
 				perf_cur->mdp_bw,
@@ -2906,7 +2906,7 @@ void mdp4_overlay_mdp_perf_upd(struct msm_fb_data_type *mfd,
 				perf_req->mdp_clk_rate;
 		}
 		if (perf_req->mdp_bw > perf_cur->mdp_bw) {
-			pr_info("%s mdp bw is changed [%d] from %d to %d\n",
+			pr_debug("%s mdp bw is changed [%d] from %d to %d\n",
 				__func__,
 				flag,
 				perf_cur->mdp_bw,
