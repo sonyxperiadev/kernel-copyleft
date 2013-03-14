dbeaf18ef779d7db045d2fe2dd65a53ee54a429b
Ashwin Chaugule <ashwinc@codeaurora.org>
2013-03-14 18:37:49 -0400

    Perf: Add L1 counters to tracepoints
    Create a new tracepoint to get L1 PMU counter
    information at every context switch. Counters
    are captured only when the tracepoint is enabled
    via perf or ftrace.
    
    Change-Id: If05527b29e04c700170cfdf9f88d300b2f558ffa
    Signed-off-by: Ashwin Chaugule <ashwinc@codeaurora.org>

--- arch/arm/mach-msm/perf_debug.c
+++ arch/arm/mach-msm/perf_debug.c
@@ -27,6 +27,7 @@ static char *descriptions =
 	"2  Perf: Toggle PMU IRQ when CPU's are hotplugged\n"
 	"3  Perf: Correct irq for CPU hotplug detection\n"
 	"4  Perf: Check perf activity on correct CPU\n"
+	"7  Perf: Add L1 counters to tracepoints\n"
 ;
 
 static ssize_t desc_read(struct file *fp, char __user *buf,
