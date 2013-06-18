d0080dd9c2d8cca3f9f8e86d0b4d5d389447ee4c
Sheetal Sahasrabudhe <sheetals@codeaurora.org>
2013-06-18 15:42:41 -0400

    Perf: Fix counts across power collapse
    After entry and exit from power collapse, the perf pm_notifier was
    calling perf_pmu_disable/enable functions from perf core. Perf core
    maintains a counter that it uses to match disable and enable calls but
    since pm_notifier is invoked asynchronously, the counter was getting a
    non-zero value which prevented the pmu from being enabled. As a result of
    this, we were seeing zero counts for some cores across power collapse.
    
    Make the architecture specific calls directly from pm_notifier.
    
    Change-Id: I744e190906a20e70e1e4798235215c385018b121
    Signed-off-by: Sheetal Sahasrabudhe <sheetals@codeaurora.org>

--- arch/arm/mach-msm/perf_debug.c
+++ arch/arm/mach-msm/perf_debug.c
@@ -28,6 +28,7 @@ static char *descriptions =
 	"3  Perf: Correct irq for CPU hotplug detection\n"
 	"4  Perf: Check perf activity on correct CPU\n"
 	"7  Perf: Add L1 counters to tracepoints\n"
+	"10 Perf: Fix counts across power collapse\n"
 ;
 
 static ssize_t desc_read(struct file *fp, char __user *buf,
