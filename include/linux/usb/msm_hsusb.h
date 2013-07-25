5a76418911e219f353763a0e289e13e3def02b9a
Manu Gautam <mgautam@codeaurora.org>
2013-07-25 12:01:02 +0530

    USB: ehci-msm-hsic: Allow enabling only SUSPEND SOF workaround
    There can be SOFs underway after suspending the port
    (setting susp bit) which leads to phy lockup or unexpected
    device disconnections on some platforms. Fix for this is to halt
    the controller before setting port suspend bit to avoid such issues.
    There are similar SOF related PHY issues for RESET and RESUME.
    There already exists a platform_data or DT parameter:
    'phy_sof_workaround' which enables all these SOF related workarounds.
    But, some hardwares have RESET and RESUME SOF issues fixed but only
    SUSPEND SOF issues is present. For these add pdata and DT parameter
    to enable only SUSPEND SOF workaround.
    
    CRs-fixed: 510136
    Change-Id: I57d7fb11616e96b606f9f0c80bf3222fca27bb5d
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -402,11 +402,20 @@ struct msm_otg {
 	unsigned int current_max;
 };
 
+/**
+ * struct msm_hsic_host_platform_data - platform device data
+ *              for msm_hsic_host driver.
+ * @phy_sof_workaround: Enable ALL PHY SOF bug related workarounds for
+		SUSPEND, RESET and RESUME.
+ * @phy_susp_sof_workaround: Enable PHY SOF workaround only for SUSPEND.
+ *
+ */
 struct msm_hsic_host_platform_data {
 	unsigned strobe;
 	unsigned data;
 	bool ignore_cal_pad_config;
 	bool phy_sof_workaround;
+	bool phy_susp_sof_workaround;
 	int strobe_pad_offset;
 	int data_pad_offset;
 
