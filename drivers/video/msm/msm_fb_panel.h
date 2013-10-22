40d83c037cbe0263ebfa20332aa55b8eeb7e1f53
Vishnuvardhan Prodduturi <vproddut@codeaurora.org>
2013-10-22 11:58:08 +0530

    msm: msm_fb: Fix Regressions because of overmap changes.
    Previous MDP overmap change is causing issues with HDMI because of
    assumptions about overmap. This patch fixes all such issues.
    
    Change-Id: Ifd2f8a0596d9df08538464bdcdd5946cf84db4d9
    Signed-off-by: Vishnuvardhan Prodduturi <vproddut@codeaurora.org>

--- drivers/video/msm/msm_fb_panel.h
+++ drivers/video/msm/msm_fb_panel.h
@@ -182,6 +182,8 @@ struct msm_panel_info {
 	struct lcdc_panel_info lcdc;
 	struct mipi_panel_info mipi;
 	struct lvds_panel_info lvds;
+	__u32 xres_aligned;
+	__u32 yres_aligned;
 };
 
 #define MSM_FB_SINGLE_MODE_PANEL(pinfo)		\
