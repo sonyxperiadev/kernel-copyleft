4ba9650562944a2cdc5c3204e02d4af1f7744b43
Nirmal Abraham <nabrah@codeaurora.org>
2014-01-08 17:15:50 +0530

    MDP: correct clock calculation logic for up-scaling scenario
    Part of yscale calculation logic in mdp4_calc_req_mdp_clk
    function is only applicable for downscale scenarios. The
    incorrect clock calculation was unnecessarily creating
    blt mode requirement even for upscale scenarios.
    
    Change-Id: I036be2ceb2f59fb159885b2d15b208706808198b
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2834,16 +2834,13 @@ static int mdp4_calc_req_mdp_clk(struct msm_fb_data_type *mfd,
 	pr_debug("%s: the right %d shifted xscale is %d.\n",
 		 __func__, shift, xscale);
 
-	if (src_h > dst_h) {
-		yscale = src_h;
-		yscale <<= shift;
-		yscale /= dst_h;
-	} else {		/* upscale */
-		yscale = dst_h;
-		yscale <<= shift;
-		yscale /= src_h;
-	}
+	if (src_h > dst_h)
+	        yscale = src_h;
+	else
+                yscale = dst_h;
 
+        yscale <<= shift;
+        yscale /= dst_h;
 	yscale *= src_w;
 	yscale /= hsync;
 
