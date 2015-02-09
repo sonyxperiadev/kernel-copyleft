58b0fd055fc2921b5a3b2fcb5e6a97262d2af2c2
Vijaya Kumar T M <vtmuni@codeaurora.org>
2015-02-09 11:47:52 +0530

    msm:pproc:Freed the vpe prameters to avoid memleak
    The vpe parameters process pp_frame and event_cmd are
    freed in failure case to avoid memleak.
    
    Change-Id: I5391f7053ae1e801f4ac40cece9786ea0c9c076d
    Signed-off-by: Shilpa Mamidi <shilpam@codeaurora.org>
    Signed-off-by: Vijaya Kumar T M <vtmuni@codeaurora.org>

--- drivers/media/video/msm/msm_vpe.c
+++ drivers/media/video/msm/msm_vpe.c
@@ -947,8 +947,12 @@ static long msm_vpe_subdev_ioctl(struct v4l2_subdev *sd,
 			pp_event_info.ack.cmd, pp_event_info.ack.status,
 			pp_event_info.ack.cookie);
 		if (copy_to_user((void __user *)v4l2_ioctl->ioctl_ptr,
-			&pp_event_info,	sizeof(struct msm_mctl_pp_event_info)))
+			&pp_event_info,	sizeof(struct msm_mctl_pp_event_info))) {
+			kfree(pp_frame_info);
+			kfree(event_qcmd);
 			pr_err("%s PAYLOAD Copy to user failed ", __func__);
+	                return -EINVAL;
+		 }
 
 		kfree(pp_frame_info);
 		kfree(event_qcmd);
