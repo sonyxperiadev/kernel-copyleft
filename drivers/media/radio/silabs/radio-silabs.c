70105823b30845fa9ebfa02d539a5dee0f1f6477
Alin Jerpelea <alin.jerpelea@sony.com>
2021-04-26 13:08:01 +0200

    Software version 58.0.A.28.60
    Open source download for Xperia 5 II (XQ-AS72)
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- drivers/media/radio/silabs/radio-silabs.c
+++ drivers/media/radio/silabs/radio-silabs.c
@@ -9,6 +9,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+/*
+ * NOTE: This file has been modified by Sony Mobile Communications Inc.
+ * Modifications are Copyright (c) 2020 Sony Mobile Communications Inc,
+ * and licensed under the license of the file.
+ */
 
 
 #define DRIVER_NAME "radio-silabs"
