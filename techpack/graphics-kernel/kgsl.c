7eb45f91036c29f21fc22d7d8d181fcf814b4a41
Alin Jerpelea <alin.jerpelea@sony.com>
2025-05-13 14:17:05 +0200

    69.0.A.2.38: Open source software for Xperia 1 VI
    Open source software for Xperia 1 VI (XQ-EC54)
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- techpack/graphics-kernel/kgsl.c
+++ techpack/graphics-kernel/kgsl.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2008-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <uapi/linux/sched/types.h>
@@ -330,6 +330,9 @@ static void kgsl_destroy_ion(struct kgsl_memdesc *memdesc)
 		struct kgsl_mem_entry, memdesc);
 	struct kgsl_dma_buf_meta *metadata = entry->priv_data;
 
+	if (memdesc->priv & KGSL_MEMDESC_MAPPED)
+		return;
+
 	if (metadata != NULL) {
 		remove_dmabuf_list(metadata);
 		dma_buf_unmap_attachment(metadata->attach, memdesc->sgt, DMA_BIDIRECTIONAL);
@@ -352,6 +355,9 @@ static void kgsl_destroy_anon(struct kgsl_memdesc *memdesc)
 	int i = 0, j;
 	struct scatterlist *sg;
 	struct page *page;
+
+	if (memdesc->priv & KGSL_MEMDESC_MAPPED)
+		return;
 
 	for_each_sg(memdesc->sgt->sgl, sg, memdesc->sgt->nents, i) {
 		page = sg_page(sg);
@@ -4036,6 +4042,14 @@ static u64 cap_alignment(struct kgsl_device *device, u64 flags)
 	return flags | FIELD_PREP(KGSL_MEMALIGN_MASK, align);
 }
 
+static u64 gpumem_max_va_size(struct kgsl_pagetable *pt, u64 flags)
+{
+	if (flags & KGSL_MEMFLAGS_FORCE_32BIT)
+		return pt->compat_va_end - pt->compat_va_start;
+
+	return pt->va_end - pt->va_start;
+}
+
 static struct kgsl_mem_entry *
 gpumem_alloc_vbo_entry(struct kgsl_device_private *dev_priv,
 		u64 size, u64 flags)
@@ -4044,6 +4058,7 @@ gpumem_alloc_vbo_entry(struct kgsl_device_private *dev_priv,
 	struct kgsl_device *device = dev_priv->device;
 	struct kgsl_memdesc *memdesc;
 	struct kgsl_mem_entry *entry;
+	struct kgsl_pagetable *pt;
 	int ret;
 
 	/* Disallow specific flags */
@@ -4063,6 +4078,12 @@ gpumem_alloc_vbo_entry(struct kgsl_device_private *dev_priv,
 
 	if ((flags & KGSL_MEMFLAGS_SECURE) && !check_and_warn_secured(device))
 		return ERR_PTR(-EOPNOTSUPP);
+
+	pt = (flags & KGSL_MEMFLAGS_SECURE) ?
+			device->mmu.securepagetable : private->pagetable;
+
+	if (!size || (size > gpumem_max_va_size(pt, flags)))
+		return ERR_PTR(-EINVAL);
 
 	flags = cap_alignment(device, flags);
 
--- techpack/kernel-modules/Android.mk
+++ techpack/kernel-modules/Android.mk
@@ -1,8 +1,27 @@
 MY_LOCAL_PATH := $(call my-dir)
 DLKM_DIR   := $(TOP)/device/qcom/common/dlkm
 
-ifeq ($(SOMC_CFG_TOUCHSCREEN_LXS),yes)
+ifeq ($(SOMC_CFG_POWERKEY_FORCECRASH),yes)
 
-include $(MY_LOCAL_PATH)/msm/lxs_ts/Android.mk
+include $(MY_LOCAL_PATH)/powerkey_forcecrash/Android.mk
 
 endif
+
+ifeq ($(SOMC_CFG_LAST_LOGS),yes)
+
+include $(MY_LOCAL_PATH)/last_logs/Android.mk
+
+endif
+
+ifeq ($(SOMC_CFG_RAMDUMP_MEM_DESC),yes)
+
+include $(MY_LOCAL_PATH)/ramdump_mem_desc/Android.mk
+
+endif
+
+ifeq ($(SOMC_CFG_RDTAGS),yes)
+
+include $(MY_LOCAL_PATH)/rdtags/Android.mk
+
+endif
+
